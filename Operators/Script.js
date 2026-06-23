a32f12482cbb2a55ddc3a87d15204f0b2dd34d62
anilpandey18 <anilpandey2063@gmail.com>
2026-06-23 15:43:34 +0545

    Data types

---
+++ Operators/Script.js
@@ -0,0 +1,85 @@
+// Arithmetic, Comparison, Logical, Assignment, Unary, Ternary
+
+// + - / * % **
+
+// 1 + 2 = 3
+// "ha" + "rsh" = "harsh"
+// This is called concatenation.
+
+// Comparison
+
+// = is used to assign a value.
+// let a = 12;  // Assignment Operator
+
+// == checks whether the values on the left and right sides are equal.
+// === checks both value and data type (strict equality).
+
+// !=
+// Example: 12 != 13
+// It means "12 is not equal to 13", so the result is true.
+
+// !== is the strict version of "not equal to".
+
+// >= Greater than or equal to
+// <= Less than or equal to
+// < Less than
+// > Greater than
+
+// Assignment Operators
+
+// let a = 12;
+// a += 3;
+// a -= 11;
+// a *= 2;
+// a /= 2;
+// a %= 3;
+
+// Logical Operators
+
+// 1. && (AND)
+// Both sides must be true.
+
+// true && true = true
+// false && true = false
+// true && false = false
+// false && false = false
+
+// 12 > 13 && 13 > 10
+// false      true
+// Result: false
+
+// 2. || (OR)
+// At least one side must be true.
+
+// 12 > 13 || 13 > 10
+// false      true
+// Result: true
+
+// 3. ! (NOT)
+// It changes a truthy value to false and a falsy value to true.
+
+// 12        -> truthy
+// !12       -> false
+// !!12      -> true
+
+// Unary Operators
+
+// +, -, !, ++, --
+
+// If you have a string that can be converted into a number,
+// place a + sign before it.
+
+// +"12"
+// Output: 12
+
+// In this topic, we also learned about increment and decrement operators.
+
+// We will also learn the types of arrays, objects, and functions.
+
+// Another concept we learned is "instance".
+// We will understand what an instance is and where it is used.
+
+// Note:
+// typeof is mostly used with primitive data types.
+
+// instanceof is mostly used with arrays, objects, and functions.
