1c0176cb6813a619ca03efe656c7b1afdc960936
anilpandey18 <anilpandey2063@gmail.com>
2026-06-24 15:20:04 +0545

    Basic about control flow

---
+++ Control Flow/script.js
@@ -0,0 +1,49 @@
+// if else else if
+// switch case
+// early return pattern
+
+// 1. if else else if
+// hami le else if jati pane banau na painxa
+
+// if (loggedin && admin){
+
+// }
+// else if (loggedin){
+
+// }
+// else {
+
+// }
+
+// 2. switch case
+
+// switch (1) {
+//     case 1:
+//         console.log("hello")
+//         break;
+//     case 2:
+//         break;
+//     case 3:
+//         break;
+//         default:
+// }
+
+// 3. early return pattern
+
+// function getval(val) {
+//     if (val < 100) return "A";
+//     else if (val < 75) return "B";
+//     else if (val < 50) return "C";
+//     else return "D";
+// }
+// console.log(getval(30));
+
+// Reverse 
+
+// function getval(val) {
+//     if (val < 25) return "D";
+//     else if (val < 50) return "C";
+//     else if (val < 75) return "B";
+//     else return "A";
+// }
+// console.log(getval(30));
